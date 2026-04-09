91edb164618231797579ee756428ffc0393ca61b
Tais993 <tijs@familiebeek.eu>
2026-04-09 13:32:08 +0200

    Added initialise command
    Initialise command should recognise the directory, upload info to the DB and create a file with the project ID. But it doesn't seem to work, so issue for later

---
+++ Photography/commands/InitialiseCommand.cs
@@ -0,0 +1,49 @@
+﻿using System.CommandLine;
+using PhotographyNET.services.interfaces;
+
+namespace PhotographyNET.commands;
+
+public class InitialiseCommand : CommandBase
+{
+
+    private IProjectResolver projectResolver;
+
+    public InitialiseCommand(IProjectResolver projectResolver)
+    {
+        this.projectResolver = projectResolver;
+    }
+    
+    
+    
+    protected override string Name => "initialise";
+    protected override string Description => "Initialises all project folders, and recognizes the current structure";
+    
+
+    private readonly Option<string> _mainFolder = new("--main-folder")
+    {
+
+        Description = "Use exact matching only"
+    };
+
+    protected override void Configure(Command command)
+    {
+        base.Configure(command);
+        
+        command.Options.Add(_mainFolder);
+    }
+    
+    
+
+    protected override int Run(ParseResult parseResult)
+    {
+        string consoleDirectory = Directory.GetCurrentDirectory();
+        string[] subdirectories = Directory.GetDirectories(consoleDirectory);
+        
+        foreach (string subdirectory in subdirectories)
+        {
+            projectResolver.initialiseExistingFolder(subdirectory);
+        }
+        
+        return 0;
+    }
+}
