f307e60c6c7b5eca47232daf8dacd1fa66dde38f
Tais993 <tijs@familiebeek.eu>
2026-04-24 08:20:38 +0200

    Fully implemented Files API
    Now all needed methods exist, IFiles.Exists, Delete, Move, everyone one might need.
    
    Any other features that are needed will just be added on the fly
    
    Closes #35; Closes #38; Closes #36; Closes #33; Closes #34; Closes #37

--- Infrastructure/filesystem/Files.cs
+++ Infrastructure/filesystem/Files.cs
@@ -2,20 +2,9 @@
 
 public class Files : IFiles
 {
-
     public string Combine(params string[] paths)
     {
         return Path.Combine(paths);
-    }
-
-    public string ReadAllText(string path)
-    {
-        return File.ReadAllText(path);
-    }
-
-    public void WriteAllText(string text, string path)
-    {
-        File.WriteAllText(path, text);
     }
 
     public string GetFileName(string path)
@@ -38,5 +27,45 @@ public class Files : IFiles
     public string[] GetDirectories(string path)
     {
         return Directory.GetDirectories(path);
+    }
+
+    public string[] GetFiles(string path)
+    {
+        return Directory.GetFiles(path);
+    }
+
+    public void WriteAllText(string text, string path)
+    {
+        File.WriteAllText(text, path);
+    }
+
+    public string ReadAllText(string path)
+    {
+        return File.ReadAllText(path);
+    }
+
+    public void DeleteFile(string path)
+    {
+        File.Delete(path);
+    }
+
+    public void FolderCreate(string path)
+    {
+        Directory.CreateDirectory(path);
+    }
+
+    public void FolderDelete(string path)
+    {
+        Directory.Delete(path);
+    }
+
+    public void MoveFile(string path, string newPath)
+    {
+        File.Move(path, newPath);
+    }
+
+    public void MoveFolder(string path, string newPath)
+    {
+        Directory.Move(path, newPath);
     }
 }
--- Infrastructure/filesystem/IFiles.cs
+++ Infrastructure/filesystem/IFiles.cs
@@ -2,11 +2,96 @@
 
 public interface IFiles
 {
+    /// <summary>
+    /// Combines all given string into a path
+    /// </summary>
+    /// <param name="paths"></param>
+    /// <returns></returns>
     string Combine(params string[] paths);
-    string ReadAllText(string path);
-    void WriteAllText(string text, string path);
+
+    /// <summary>
+    /// Gets the filename from any given path
+    /// </summary>
+    /// <param name="path"></param>
+    /// <returns></returns>
     string GetFileName(string path);
+
+    /// <summary>
+    /// Gets the final directory from any given path (if a filename is the last mentioned item, that will be returned instead)
+    /// </summary>
+    /// <param name="path"></param>
+    /// <returns></returns>
     string GetPathEnd(string path);
+
+    /// <summary>
+    /// Verifies whenever any given path exists on the drive, both directories and specific files work.
+    /// </summary>
+    /// <param name="path"></param>
+    /// <returns></returns>
     bool Exists(string path);
+
+
+    /// <summary>
+    /// Returns all the subdirectories from any given path.
+    /// </summary>
+    /// <param name="path"></param>
+    /// <returns></returns>
     string[] GetDirectories(string path);
+
+    /// <summary>
+    /// Returns all files listed in the given path
+    /// </summary>
+    /// <param name="path"></param>
+    /// <returns></returns>
+    string[] GetFiles(string path);
+
+
+    /// <summary>
+    /// Creates and or writes all given text onto the given path
+    /// </summary>
+    /// <param name="text"></param>
+    /// <param name="path"></param>
+    void WriteAllText(string text, string path);
+
+    /// <summary>
+    /// Reads all text from a file on the given path
+    /// </summary>
+    /// <param name="path"></param>
+    /// <returns></returns>
+    string ReadAllText(string path);
+
+    void DeleteFile(string path);
+
+
+
+    /// <summary>
+    /// Creates a folder at the given path
+    /// </summary>
+    /// <param name="path"></param>
+    void FolderCreate(string path);
+
+    /// <summary>
+    /// Deletes an empty directory from the given path
+    /// </summary>
+    /// <param name="path"></param>
+    void  FolderDelete(string path);
+
+
+
+
+    /// <summary>
+    /// Moves the given file from its origin path to its destination path.
+    /// </summary>
+    /// <param name="path">The current path</param>
+    /// <param name="newPath">The new path</param>
+    void MoveFile(string path, string newPath);
+
+    /// <summary>
+    /// Moves the given folder (or file) from its origin path to its destination path
+    /// </summary>
+    /// <param name="path">The current path</param>
+    /// <param name="newPath">The new path</param>
+    void MoveFolder(string path, string newPath);
+
+
 }
