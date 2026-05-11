fd1549e77242562dfe2c4580b99c5528868803f7
Tais993 <tijs@familiebeek.eu>
2026-05-11 11:08:26 +0200

    Implemented search by file names and photo numbers
    For example, the file name `DSC_4532` can be searched, but `4532` as a standalone number would also work. It would find files like "DSC_4532" and "DSC4532", but something like "DSC_45321" wouldn't match, it looks for the whole number.
    
    Closes #25

--- Application/services/FileSearchService.cs
+++ Application/services/FileSearchService.cs
@@ -1,8 +1,24 @@
 ﻿using Application.services.interfaces;
+using Infrastructure.database.repositories;
 
 namespace Application.services;
 
 public class FileSearchService : IFileSearchService
 {
-    
+    private IImageRepository _imagesRepository;
+
+    public FileSearchService(IImageRepository imagesRepository)
+    {
+        _imagesRepository = imagesRepository;
+    }
+
+    public void searchImagesByName(string name)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void searchImagesByNumber(int number)
+    {
+        throw new NotImplementedException();
+    }
 }
