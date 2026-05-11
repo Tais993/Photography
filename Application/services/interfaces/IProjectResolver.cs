09c17bfaafe6ade1164d4e344ac64a4a103c8780
Tais993 <tijs@familiebeek.eu>
2026-05-11 15:23:39 +0200

    Added documentation and privated a method

--- Application/services/interfaces/IProjectResolver.cs
+++ Application/services/interfaces/IProjectResolver.cs
@@ -4,7 +4,17 @@ namespace Application.services.interfaces;
 
 public interface IProjectResolver
 {
+    /// <summary>
+    /// Based on the given directory, and projects at that directory are searched for and returned.
+    /// </summary>
+    /// <param name="directory">The directory for which to look </param>
+    /// <returns></returns>
     public Project resolveProject(string directory);
 
+    /// <summary>
+    /// Based on the given directory, looks for any projects and or its subprojects to load their info into the database.
+    /// The images will also be stored in the database for searching, an uninitialized project has no use or functionalities.
+    /// </summary>
+    /// <param name="projectDirectory"></param>
     public void initialiseExistingFolder(string projectDirectory);
 }
