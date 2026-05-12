f80e807d6c976e8880a4b7f1996538cd09a41200
Tais993 <tijs@familiebeek.eu>
2026-05-12 17:07:22 +0200

    Applied naming conventions for FileSearchService and added methods

--- Application/services/FileSearchService.cs
+++ Application/services/FileSearchService.cs
@@ -6,32 +6,44 @@ namespace Application.services;
 
 public class FileSearchService : IFileSearchService
 {
-    private IImageRepository _imagesRepository;
+    private readonly IImageRepository _imagesRepository;
 
     public FileSearchService(IImageRepository imagesRepository)
     {
         _imagesRepository = imagesRepository;
     }
 
-    public List<Image> searchImagesByNameOrNumber(string name)
+    public List<Image> SearchImagesByNameOrNumber(int projectId, string name)
     {
-        if (int.TryParse(name, out _))
-        {
-            return searchImagesByNumber(name);
-        }
-        else
-        {
-            return searchImagesByName(name);
-        }
+        if (int.TryParse(name, out _)) return SearchImagesByNumber(projectId, name);
+
+        return SearchImagesByName(projectId, name);
     }
 
-    public List<Image> searchImagesByName(string name)
+    public List<Image> SearchImagesByNameOrNumber(string name)
+    {
+        if (int.TryParse(name, out _)) return SearchImagesByNumber(name);
+
+        return SearchImagesByName(name);
+    }
+
+    public List<Image> SearchImagesByName(string name)
     {
         return _imagesRepository.GetImagesByFileName(name);
     }
 
-    public List<Image> searchImagesByNumber(string number)
+    public List<Image> SearchImagesByName(int projectId, string name)
+    {
+        return _imagesRepository.GetImagesByFileName(projectId, name);
+    }
+
+    public List<Image> SearchImagesByNumber(string number)
     {
         return _imagesRepository.GetImagesByPhotoNumber(number);
+    }
+
+    public List<Image> SearchImagesByNumber(int projectId, string number)
+    {
+        return _imagesRepository.GetImagesByPhotoNumber(projectId, number);
     }
 }
