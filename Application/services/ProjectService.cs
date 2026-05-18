fe4c79454bf4bf0f579641deddd81435557b4641
Tais993 <tijs@familiebeek.eu>
2026-05-18 16:12:45 +0200

    Additional styling improvements and added additional ResolveProjectId method

--- Application/services/ProjectService.cs
+++ Application/services/ProjectService.cs
@@ -11,10 +11,9 @@ public partial class ProjectService : IProjectService
 {
     public static readonly Regex ProjectNameRegex = GeneratedProjectNameRegex();
     public const string ProjectInfoFile = "project.info";
-
-    private readonly IProjectRepository _projectRepository;
-    private readonly IImageRepository _imageRepository;
     private readonly ILogger<ProjectService> _logger;
+    private readonly IImageRepository _imageRepository;
+    private readonly IProjectRepository _projectRepository;
     private readonly IFiles _files;
 
 
@@ -30,7 +29,7 @@ public partial class ProjectService : IProjectService
 
     public int ResolveProjectId(string directory)
     {
-        var projectInfoLocation = _files.Combine(directory, ProjectInfoFile);
+        string projectInfoLocation = _files.Combine(directory, ProjectInfoFile);
 
         if (!_files.Exists(projectInfoLocation))
         {
@@ -44,6 +43,16 @@ public partial class ProjectService : IProjectService
         return id;
     }
 
+    public int ResolveProjectId(string directory, int possibleEmptyProjectId)
+    {
+        if (possibleEmptyProjectId == 0)
+        {
+            return ResolveProjectId(Directory.GetCurrentDirectory());
+        }
+
+        return possibleEmptyProjectId;
+    }
+
     public Project ResolveProject(string directory)
     {
         return _projectRepository.GetById(ResolveProjectId(directory));
@@ -52,7 +61,7 @@ public partial class ProjectService : IProjectService
 
     public void InitialiseExistingFolder(string projectDirectory)
     {
-        var pathEnd = _files.GetPathEnd(projectDirectory);
+        string pathEnd = _files.GetPathEnd(projectDirectory);
 
         _logger.LogInformation("folder name: {FolderName}", pathEnd);
 
@@ -60,17 +69,14 @@ public partial class ProjectService : IProjectService
         if (pathEnd.StartsWith('.'))
         {
             _logger.LogInformation("it is a collection folder");
-            foreach (string directory in _files.GetDirectories(projectDirectory))
-            {
-                InitialiseExistingFolder(directory);
-            }
+            foreach (string directory in _files.GetDirectories(projectDirectory)) InitialiseExistingFolder(directory);
 
             // This is a collection folder for example all concerts photographed at De Pul
             // For now this has no additional functionality and we will ignore it
         }
         else
         {
-            var match = ProjectNameRegex.Match(pathEnd);
+            Match match = ProjectNameRegex.Match(pathEnd);
 
             if (match.Success)
             {
@@ -84,7 +90,7 @@ public partial class ProjectService : IProjectService
                     return;
                 }
 
-                var project = ToProject(projectDirectory, match);
+                Project project = ToProject(projectDirectory, match);
 
                 project = _projectRepository.Insert(project);
                 _files.WriteAllText(project.Id + "", projectInfoLocation);
@@ -97,6 +103,7 @@ public partial class ProjectService : IProjectService
 
 
                 _logger.LogDebug("Going through all images now");
+
                 foreach (string subDirectory in _files.GetDirectories(projectDirectory))
                 {
                     InitializeImages(projectDirectory, subDirectory, project.Id.Value);
@@ -105,34 +112,31 @@ public partial class ProjectService : IProjectService
                 _logger.LogDebug("All images initialized");
                 _logger.LogInformation("Successfully initialized project and all images");
             }
-            else
-            {
-                // What to do?
-            }
+            // What to do?
         }
     }
 
     private static Project ToProject(string subdirectory, Match match)
     {
-        var dateOnly = new DateOnly(int.Parse(match.Groups[1].Value), int.Parse(match.Groups[2].Value),
+        DateOnly dateOnly = new DateOnly(int.Parse(match.Groups[1].Value), int.Parse(match.Groups[2].Value),
             int.Parse(match.Groups[3].Value));
         Project project = new Project(null, match.Groups[4].Value, subdirectory, dateOnly);
         return project;
     }
 
     /// <summary>
-    /// This method expects a project's subfolder already, and this also
+    ///     This method expects a project's subfolder already, and this also
     /// </summary>
     /// <param name="projectDirectory">the project directory</param>
     /// <param name="projectSubDirectory">a subfolder from within a project that contains images</param>
     /// <param name="projectId"></param>
     private void InitializeImages(string projectDirectory, string projectSubDirectory, int projectId)
     {
-        foreach (var filePath in _files.GetFiles(projectSubDirectory))
+        foreach (string filePath in _files.GetFiles(projectSubDirectory))
         {
-            var fileExtension = _files.GetFileExtension(filePath);
-            var fileName = _files.GetFileName(filePath).Replace(fileExtension, "");
-            var relativeFilePath = _files.GetRelativePath(projectDirectory, filePath);
+            string fileExtension = _files.GetFileExtension(filePath);
+            string fileName = _files.GetFileName(filePath).Replace(fileExtension, "");
+            string relativeFilePath = _files.GetRelativePath(projectDirectory, filePath);
 
             Image image = new Image(projectId, fileName, fileExtension, relativeFilePath);
 
