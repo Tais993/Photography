17c1006078696f194841bca14505226f0b5154e8
Tais993 <tijs@familiebeek.eu>
2026-05-18 16:12:45 +0200

    Additional styling improvements and added additional ResolveProjectId method

--- Application/services/interfaces/IProjectService.cs
+++ Application/services/interfaces/IProjectService.cs
@@ -3,14 +3,14 @@
 namespace Application.services.interfaces;
 
 /// <summary>
-/// The project services handles with everything project related.
-/// Initializing and resolving the most predominant functions.
+///     The project services handles with everything project related.
+///     Initializing and resolving the most predominant functions.
 /// </summary>
 public interface IProjectService
 {
     /// <summary>
-    /// Based on a given directory, resolves any projects that can be found within the main, and or its parent folder.
-    /// If none is found, it'll return "0".
+    ///     Based on a given directory, resolves any projects that can be found within the main, and or its parent folder.
+    ///     If none is found, it'll return "0".
     /// </summary>
     /// <param name="directory"></param>
     /// <returns></returns>
@@ -18,7 +18,18 @@ public interface IProjectService
     public int ResolveProjectId(string directory);
 
     /// <summary>
-    /// Based on a given directory, resolves any projects that can be found within the main, and or its parent folder.
+    ///     This resolves the given directory, but only if the given project ID is 0.
+    ///     Based on a given directory, resolves any projects that can be found within the main, and or its parent folder.
+    ///     If none is found, it'll return "0".
+    /// </summary>
+    /// <param name="directory"></param>
+    /// <param name="possiblyEmptyProjectId"></param>
+    /// <returns></returns>
+    /// <exception cref="InvalidOperationException"></exception>
+    public int ResolveProjectId(string directory, int possiblyEmptyProjectId);
+
+    /// <summary>
+    ///     Based on a given directory, resolves any projects that can be found within the main, and or its parent folder.
     /// </summary>
     /// <param name="directory"></param>
     /// <returns></returns>
@@ -26,13 +37,12 @@ public interface IProjectService
     public Project ResolveProject(string directory);
 
     /// <summary>
-    /// Initialises the given projects directory into the database,
-    /// additionally this adds a file to the filesystem to remember the projects ID.
-    /// Any images from within the project will also be loaded in with its metadata saved into the database.
-    ///
-    /// Any collection folders are ignored for now, additionally if a subfolder was given its ignored as its not recognized as a project folder.
-    ///
-    /// In future versions this method will run recursively, including for any sub/collection folders.
+    ///     Initialises the given projects directory into the database,
+    ///     additionally this adds a file to the filesystem to remember the projects ID.
+    ///     Any images from within the project will also be loaded in with its metadata saved into the database.
+    ///     Any collection folders are ignored for now, additionally if a subfolder was given its ignored as its not recognized
+    ///     as a project folder.
+    ///     In future versions this method will run recursively, including for any sub/collection folders.
     /// </summary>
     /// <param name="projectDirectory"></param>
     public void InitialiseExistingFolder(string projectDirectory);
