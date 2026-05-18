fb403426602e18e1df71408135fe2cfd4cbee3ee
Tais993 <tijs@familiebeek.eu>
2026-05-18 13:53:58 +0200

    Added the project metadata service
    This also fixes part of #57 and #58
    
    Closes #2

---
+++ Application/services/ProjectMetadataService.cs
@@ -0,0 +1,79 @@
+﻿using Application.services.interfaces;
+using Domain.entities;
+using Infrastructure.database.repositories;
+
+namespace Application.services;
+
+public class ProjectMetadataService : IProjectMetadataService
+{
+    private readonly MetadataRepository _metadataRepository;
+    private readonly ProjectMetadataRepository _projectMetadataRepository;
+
+    public ProjectMetadataService(ProjectMetadataRepository projectMetadataRepository,
+        MetadataRepository metadataRepository)
+    {
+        _projectMetadataRepository = projectMetadataRepository;
+        _metadataRepository = metadataRepository;
+    }
+
+    public Metadata CreateMetadata(string metadataKey, string metadataType, string displayName, string description)
+    {
+        return CreateMetadata(new Metadata(0, metadataKey, metadataType, displayName, description));
+    }
+
+    public Metadata CreateMetadata(Metadata metadata)
+    {
+        return _metadataRepository.Insert(metadata);
+    }
+
+    public Metadata? GetMetadata(Metadata metadata)
+    {
+        if (metadata.Id == null) throw new ArgumentNullException("metadata.Id");
+
+        return GetMetadata((int)metadata.Id);
+    }
+
+    public Metadata? GetMetadata(int projectId)
+    {
+        return _metadataRepository.GetById(projectId);
+    }
+
+
+    public void DeleteMetadata(Metadata metadata)
+    {
+        if (metadata.Id == null) throw new ArgumentNullException("metadata.Id");
+
+        DeleteMetadata((int)metadata.Id);
+    }
+
+
+    public void DeleteMetadata(int projectId)
+    {
+        _metadataRepository.DeleteById(projectId);
+    }
+
+
+    public List<ProjectMetadata> GetProjectMetadata(Project project)
+    {
+        if (project.Id == null) throw new ArgumentNullException("project.Id");
+
+        return GetProjectMetadata((int)project.Id);
+    }
+
+    public List<ProjectMetadata> GetProjectMetadata(int projectId)
+    {
+        return _projectMetadataRepository.GetAllByProjectId(projectId);
+    }
+
+    public void AddMetadataToProject(int projectId, int metadataId, string value)
+    {
+        var projectMetadata = new ProjectMetadata(projectId, metadataId, value);
+
+        _projectMetadataRepository.Insert(projectMetadata);
+    }
+
+    public void DeleteMetadataFromProject(int projectId, int metadataId)
+    {
+        _projectMetadataRepository.DeleteByKey(projectId, metadataId);
+    }
+}
