36a4b693aa35c66ae704c4af1c94fb76c6304cff
Tais993 <tijs@familiebeek.eu>
2026-05-18 13:51:30 +0200

    Changed Metadata and ProjectMetadata domain objects

--- Domain/entities/Metadata.cs
+++ Domain/entities/Metadata.cs
@@ -1,10 +1,11 @@
 ﻿namespace Domain.entities;
 
-public class Metadata(int id, string metadataKey, string metadataType, string displayName, string description) : IIdEntity
+public class Metadata(int id, string metadataKey, string metadataType, string displayName, string description)
+    : IIdEntity
 {
     public int? Id { get; } = id;
-    public string MetadataKey { get; } = metadataKey;
-    public string MetadataType { get; } = metadataType;
-    public string DisplayName { get; } = displayName;
-    public string Description { get; } = description;
+    public string MetadataKey = metadataKey;
+    public string MetadataType = metadataType;
+    public string DisplayName = displayName;
+    public string Description = description;
 }
