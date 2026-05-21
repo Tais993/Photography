68a8129ed41b381a4dd255023bb8aaa1a6aefac4
Tais993 <tijs@familiebeek.eu>
2026-05-21 22:56:19 +0200

    Added a CopyFile and GetFileNameWithoutExtension to Files

--- Infrastructure/filesystem/IFiles.cs
+++ Infrastructure/filesystem/IFiles.cs
@@ -3,44 +3,53 @@
 public interface IFiles
 {
     /// <summary>
-    /// Combines all given string into a path
+    ///     Combines all given string into a path
     /// </summary>
     /// <param name="paths"></param>
     /// <returns></returns>
     string Combine(params string[] paths);
 
     /// <summary>
-    /// Gets the filename from any given path, including the extension
+    ///     Gets the filename from any given path, including the extension
     /// </summary>
     /// <param name="path"></param>
     /// <returns></returns>
     string GetFileName(string path);
 
     /// <summary>
-    /// Gets the extention of the given file's path
+    ///     Gets the filename from any given path, excluding the extension
+    /// </summary>
+    /// <param name="path"></param>
+    /// <returns></returns>
+    string GetFileNameWithoutExtension(string path);
+
+
+    /// <summary>
+    ///     Gets the extention of the given file's path
     /// </summary>
     /// <param name="path"></param>
     /// <returns></returns>
     public string GetFileExtension(string path);
 
     /// <summary>
-    /// Gets the final directory from any given path (if a filename is the last mentioned item, that will be returned instead)
-    /// If the given path included a filename at the end, the file namne including the extension will be returned.
+    ///     Gets the final directory from any given path (if a filename is the last mentioned item, that will be returned
+    ///     instead)
+    ///     If the given path included a filename at the end, the file namne including the extension will be returned.
     /// </summary>
     /// <param name="path"></param>
     /// <returns></returns>
     string GetPathEnd(string path);
 
     /// <summary>
-    /// Creates
+    ///     Creates
     /// </summary>
     /// <param name="relativeTo"></param>
     /// <param name="path"></param>
     /// <returns></returns>
     public string GetRelativePath(string relativeTo, string path);
-    
+
     /// <summary>
-    /// Verifies whenever any given path exists on the drive, both directories and specific files work.
+    ///     Verifies whenever any given path exists on the drive, both directories and specific files work.
     /// </summary>
     /// <param name="path"></param>
     /// <returns></returns>
@@ -48,14 +57,14 @@ public interface IFiles
 
 
     /// <summary>
-    /// Returns all the subdirectories from any given path.
+    ///     Returns all the subdirectories from any given path.
     /// </summary>
     /// <param name="path"></param>
     /// <returns></returns>
     string[] GetDirectories(string path);
 
     /// <summary>
-    /// Returns all files listed in the given path
+    ///     Returns all files listed in the given path
     /// </summary>
     /// <param name="path"></param>
     /// <returns></returns>
@@ -63,14 +72,14 @@ public interface IFiles
 
 
     /// <summary>
-    /// Creates and or writes all given text onto the given path
+    ///     Creates and or writes all given text onto the given path
     /// </summary>
     /// <param name="text"></param>
     /// <param name="path"></param>
     void WriteAllText(string text, string path);
 
     /// <summary>
-    /// Reads all text from a file on the given path
+    ///     Reads all text from a file on the given path
     /// </summary>
     /// <param name="path"></param>
     /// <returns></returns>
@@ -81,33 +90,37 @@ public interface IFiles
 
 
     /// <summary>
-    /// Creates a folder at the given path
+    ///     Creates a folder at the given path
     /// </summary>
     /// <param name="path"></param>
     void FolderCreate(string path);
 
     /// <summary>
-    /// Deletes an empty directory from the given path
+    ///     Deletes an empty directory from the given path
     /// </summary>
     /// <param name="path"></param>
-    void  FolderDelete(string path);
-
-
+    void FolderDelete(string path);
 
 
     /// <summary>
-    /// Moves the given file from its origin path to its destination path.
+    ///     Copy the given file from its origin path to its destination path.
+    /// </summary>
+    /// <param name="path">The current path</param>
+    /// <param name="newPath">The copied to path</param>
+    void CopyFile(string path, string newPath);
+
+
+    /// <summary>
+    ///     Moves the given file from its origin path to its destination path.
     /// </summary>
     /// <param name="path">The current path</param>
     /// <param name="newPath">The new path</param>
     void MoveFile(string path, string newPath);
 
     /// <summary>
-    /// Moves the given folder (or file) from its origin path to its destination path
+    ///     Moves the given folder (or file) from its origin path to its destination path
     /// </summary>
     /// <param name="path">The current path</param>
     /// <param name="newPath">The new path</param>
     void MoveFolder(string path, string newPath);
-
-
 }
