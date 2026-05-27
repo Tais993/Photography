ea5495b18a5f4ed5b2a5963076af1101d25d4cdd
Tais993 <tijs@familiebeek.eu>
2026-05-27 10:08:56 +0200

    Added image selection functionality to database and application layer
    Closes #28
    Closes #29
    Closes #30

---
+++ Infrastructure/database/migrations/V1_5__added_selection_column.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE selection_session
+(
+    id         serial PRIMARY KEY,
+    project_id integer      NOT NULL,
+    name       varchar(255) NOT NULL,
+    created_at timestamp    NOT NULL DEFAULT now(),
+
+    FOREIGN KEY (project_id) REFERENCES project (id) ON DELETE CASCADE
+);
+
+CREATE TABLE selection_session_image
+(
+    selection_session_id integer NOT NULL,
+    image_id             integer NOT NULL,
+
+    PRIMARY KEY (selection_session_id, image_id),
+
+    FOREIGN KEY (selection_session_id) REFERENCES selection_session (id) ON DELETE CASCADE,
+    FOREIGN KEY (image_id) REFERENCES image (id) ON DELETE CASCADE
+);
