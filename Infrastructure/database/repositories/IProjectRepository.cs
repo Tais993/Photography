2d80f625982b255c40d083eabc12b252a034472c
Tais993 <tijs@familiebeek.eu>
2026-05-28 12:33:12 +0200

    Added GetProjectImageCount

--- Infrastructure/database/repositories/IProjectRepository.cs
+++ Infrastructure/database/repositories/IProjectRepository.cs
@@ -9,4 +9,8 @@ public interface IProjectRepository
     Project Insert(Project project);
     void Update(Project project);
     void DeleteById(int id);
+
+    int GetProjectImageCount(int projectId);
+    
+    public List<Project> SearchProjects(ProjectSearchSettings projectSearchSettings);
 }
