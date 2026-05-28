c2ad46f1f9f9d84792b239548d7bc77505234e13
Tais993 <tijs@familiebeek.eu>
2026-05-28 12:41:53 +0200

    Removed unused methods on the ImageRepository

--- Infrastructure/database/repositories/IImageRepository.cs
+++ Infrastructure/database/repositories/IImageRepository.cs
@@ -5,10 +5,6 @@ namespace Infrastructure.database.repositories;
 public interface IImageRepository
 {
     public Image GetById(int id);
-    public List<Image> GetImagesByFileName(int projectId, string fileName);
-    public List<Image> GetImagesByFileName(string fileName);
-    public List<Image> GetImagesByPhotoNumber(int projectId, string fileNumber);
-    public List<Image> GetImagesByPhotoNumber(string fileNumber);
     public List<Image> GetAll();
     public List<Image> GetAllByIds(int[] imageIds);
     public List<Image> GetAllByProjectId(int projectId);
