85178383ea2fe4fd6601695690bfa04b31012230
Tais993 <tijs@familiebeek.eu>
2026-05-28 10:29:39 +0200

    Added OpenImage method for opening images in Irfanview

--- Application/services/IrfanviewService.cs
+++ Application/services/IrfanviewService.cs
@@ -1,4 +1,5 @@
-﻿using Infrastructure.filesystem;
+﻿using System.Diagnostics;
+using Infrastructure.filesystem;
 using Infrastructure.irfanview;
 
 namespace Application.services;
@@ -31,5 +32,18 @@ public class IrfanviewService
         }
 
         return null;
+    }
+
+    public void OpenImage(string irfanviewPath, string fullPath)
+    {
+        string irfanViewPath = @"C:\Program Files\IrfanView\i_view64.exe";
+        string imagePath = @"C:\Users\tijs\OneDrive\Desktop\Backup important\Picturestesting\2026-01-29-Selena\Original\DSC_7654.JPG";
+
+        Process.Start(new ProcessStartInfo
+        {
+            FileName = irfanviewPath,
+            Arguments = $"\"{fullPath}\"",
+            UseShellExecute = false
+        });
     }
 }
