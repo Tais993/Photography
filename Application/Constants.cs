efc2dfa1d2db9577ce55f5526f9a5cd27db3d618
Tais993 <tijs@familiebeek.eu>
2026-05-28 10:27:45 +0200

    Added Constants class, this contains a list of image types for raw and normal images.

---
+++ Application/Constants.cs
@@ -0,0 +1,90 @@
+﻿namespace Application;
+
+public class Constants
+{
+    public static readonly HashSet<string> ImageFileTypes = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+    {
+        ".jpg",
+        ".jpeg",
+
+        ".png",
+        "webp",
+    };
+    
+    public static readonly HashSet<string> RawFileTypes = new(StringComparer.OrdinalIgnoreCase)
+    {
+        // Adobe
+        ".dng",
+        // Arri Alexa
+        ".ari",
+        // Blackmagic Design
+        ".braw",
+        // Canon
+        ".crw",
+        ".cr2",
+        ".cr3",
+        // Casio
+        ".bay",
+        // Cintel
+        ".cri",
+        // Epson
+        ".erf",
+        // Fujifilm
+        ".raf",
+        // GoPro
+        ".gpr",
+        // Hassalblad
+        ".3fr",
+        ".fff",
+        // intoPIX
+        ".tco",
+        // JPEG
+        ".jxs",
+        // Kodak
+        ".dcs",
+        ".dcr",
+        ".drf",
+        ".k25",
+        ".kdc",
+        // Leaf
+        ".mos",
+        // Leica
+        ".raw",
+        ".rwl",
+        // Logitech
+        ".pxn",
+        // Mamiya
+        ".mef",
+        // Minolta
+        ".mdc", ".mrw",
+        // Nikon
+        ".nef",
+        ".nrw",
+        // Olympus
+        ".orf",
+        // Panasonic
+        ".rw2",
+        // Pentax
+        ".pef",
+        ".ptx",
+        // Phase One
+        ".cap",
+        ".iiq",
+        ".eip",
+        // Rawzor
+        ".rwz",
+        // RED
+        ".r3d",
+        // Samsung
+        ".srw",
+        // Sigma
+        ".x3f",
+        // Sony
+        ".arw",
+        ".srf",
+        ".sr2",
+
+        // Not Sure
+        ".data", ".obm", ".tif",
+    };
+}
