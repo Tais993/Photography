cdf559c89258a868c44795bbcde8237f2b882e9c
Tais993 <tijs@familiebeek.eu>
2026-05-31 17:22:07 +0200

    Added ImageView and added lots of JS to the SelectionIndex

---
+++ Website/Pages/Selection/_ImageView.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Website.Pages.Selection;
+
+public class _ImageView : PageModel
+{
+    
+    [BindProperty(SupportsGet = true)] public bool Selected { get; set; }
+    [BindProperty(SupportsGet = true)] public int? ImageId { get; set; }
+    [BindProperty(SupportsGet = true)] public string? FileType { get; set; }
+    [BindProperty(SupportsGet = true)] public string? FileName { get; set; }
+    [BindProperty(SupportsGet = true)] public string? RelationalFilePath { get; set; }
+
+
+    public void OnGet()
+    {
+        
+    }
+}
