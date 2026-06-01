60290371eb2f4463e1f7ef451a604b7175ba9abf
Tais993 <tijs@familiebeek.eu>
2026-06-01 10:59:44 +0200

    Added interfaces for some services

---
+++ Application/services/interfaces/IImageSelectionService.cs
@@ -0,0 +1,23 @@
+﻿using Domain.entities;
+
+namespace Application.services;
+
+public interface IImageSelectionService
+{
+    SelectionSession StartSession(Project project);
+    SelectionSession StartSession(int projectId, string? sessionName);
+    SelectionSession GetOrStartSession(Project project);
+    SelectionSession GetOrStartSession(int projectId, string? sessionName);
+    int GetSessionId(int projectId);
+    void RemoveSession(Project project);
+    void RemoveSession(int projectId);
+    void ClearSession(Project project);
+    void ClearSession(int projectId);
+    void SelectImage(Image image);
+    void AddImageToSelection(int sessionId, int imageId);
+    void RemoveImageFromSelection(int sessionId, int imageId);
+    SelectionSession GetSessionImages(Project project);
+    SelectionSession GetSessionImages(int projectId);
+    bool ImageIsSelected(int sessionId, int imageId);
+    bool ToggleImageSelection(int sessionId, int imageId);
+}
