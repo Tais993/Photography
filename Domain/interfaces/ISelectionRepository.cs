eca8c7f6173381c80812e91c672d0465e5c61351
Tais993 <tijs@familiebeek.eu>
2026-06-01 10:51:20 +0200

    Added interfaces for other repositories

---
+++ Domain/interfaces/ISelectionRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain.entities;
+
+namespace Infrastructure.database.repositories;
+
+public interface ISelectionRepository
+{
+    SelectionSession StartSession(int projectId, string sessionName);
+    SelectionSession GetOrStartSession(int projectId, string sessionName);
+    void RemoveSession(int projectId);
+    void ClearSession(int projectId);
+    SelectionSession GetSessionById(int id);
+    int GetSessionIdByProjectId(int projectId);
+    bool ImageIsSelected(int sessionId, int imageId);
+    void AddImageToProjectSelection(int selectionId, int imageId);
+    void RemoveImageFromProjectSelection(int projectId, int imageId);
+    SelectionSession GetByProject(int projectId);
+    void DeleteExpiredSessions();
+}
