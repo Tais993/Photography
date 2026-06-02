79a3f0ccf1e38527db0196ecc4a1180873c722b1
Tais993 <tijs@familiebeek.eu>
2026-06-02 16:29:16 +0200

    Added the ProjectView and improved the selected project logic

---
+++ Website/Pages/Projects/_ProjectView.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Website.Pages.Projects;
+
+public class _ProjectView : PageModel
+{
+    [BindProperty(SupportsGet = true)] public bool Selected { get; set; }
+    [BindProperty(SupportsGet = true)] public int? Id { get; set;  }
+    [BindProperty(SupportsGet = true)] public string? Name { get; set; }
+    [BindProperty(SupportsGet = true)] public string? Path { get; set; }
+    [BindProperty(SupportsGet = true)] public DateOnly? EventDate { get; set; }
+
+    public void OnGet()
+    {
+    }
+}
