a6923598d6a1e7570dafea6a7720cf432e42442f
Tais993 <tijs@familiebeek.eu>
2026-06-03 13:30:30 +0200

    Added Parent project ID to the search

--- Domain/entities/search/ProjectSearchSettings.cs
+++ Domain/entities/search/ProjectSearchSettings.cs
@@ -6,9 +6,10 @@ public class ProjectSearchSettings
     public int? ProjectId { get; set; }
     public string? ProjectName { get; set; }
     public string? ProjectPath { get; set; }
+    public int? ParentProjectId { get; set; }
 
     public override string ToString()
     {
-        return $"ProjectId: {ProjectId}, ProjectName: {ProjectName}, Path: {ProjectPath}";
+        return $"ProjectId: {ProjectId}, ProjectName: {ProjectName}, Path: {ProjectPath},  ParentProjectId: {ParentProjectId}";
     }
 }
