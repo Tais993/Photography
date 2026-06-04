acb63a6e31bfcc88390de3b27d681acc24f532dc
Tais993 <tijs@familiebeek.eu>
2026-06-04 22:20:33 +0200

    Added testing for the constants (regex specifically)
    #65

--- Tests/Application/ConstantsTest.cs
+++ Tests/Application/ConstantsTest.cs
@@ -1,4 +1,5 @@
-﻿using Application;
+﻿using System.Text.RegularExpressions;
+using Application;
 
 namespace Tests;
 
@@ -7,9 +8,33 @@ namespace Tests;
 public class ConstantsTest
 {
 
-    [Test]
-    public void METHOD()
+    [TestCase("2024-07-04-Merijn", "2024", "07", "04", "Merijn")]
+    [TestCase("2026-01-15-Antwerpen", "2026", "01", "15", "Antwerpen")]
+    [TestCase("2026-06-01-Ardennen", "2026", "06", "01", "Ardennen")]
+    public void ProjectNameRegex_MatchesValidProjectFolder(
+        string folderName,
+        string expectedYear,
+        string expectedMonth,
+        string expectedDay,
+        string expectedProjectName)
     {
-        
+        Match match = Constants.ProjectNameRegex.Match(folderName);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(match.Success, Is.True);
+            Assert.That(match.Groups[1].Value, Is.EqualTo(expectedYear));
+            Assert.That(match.Groups[2].Value, Is.EqualTo(expectedMonth));
+            Assert.That(match.Groups[3].Value, Is.EqualTo(expectedDay));
+            Assert.That(match.Groups[4].Value, Is.EqualTo(expectedProjectName));
+        });
+    }
+
+    [Test]
+    public void ProjectNameRegex_DoesNotMatchInvalidProjectFolder()
+    {
+        Match match = Constants.ProjectNameRegex.Match("invalid-folder-name");
+
+        Assert.That(match.Success, Is.False);
     }
 }
