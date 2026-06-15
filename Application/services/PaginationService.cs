5980470984290a48f431698c592feba28170747b
Tais993 <tijs@familiebeek.eu>
2026-06-15 13:48:25 +0200

    Added Pagination helper method

---
+++ Application/services/PaginationService.cs
@@ -0,0 +1,46 @@
+﻿using Domain.entities.search;
+
+namespace Application.services;
+
+public class PaginationService
+{
+    public static PaginatedResult<T> Paginate<T>(IEnumerable<T> items, SearchSettings searchSettings)
+    {
+        int pageNumber = searchSettings.PageNumber;
+        int pageSize = searchSettings.PageSize;
+
+        if (pageNumber < 1)
+        {
+            pageNumber = 1;
+        }
+
+        if (pageSize < 1)
+        {
+            pageSize = 60;
+        }
+
+        List<T> itemList = items.ToList();
+
+        int totalItems = itemList.Count;
+        int totalPages = (int)Math.Ceiling(totalItems / (double)pageSize);
+
+        if (pageNumber > totalPages && totalPages > 0)
+        {
+            pageNumber = totalPages;
+        }
+
+        List<T> pagedItems = itemList
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize)
+            .ToList();
+
+        return new PaginatedResult<T>
+        {
+            Items = pagedItems,
+            PageNumber = pageNumber,
+            PageSize = pageSize,
+            TotalItems = totalItems,
+            TotalPages = totalPages
+        };
+    }
+}
