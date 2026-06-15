d26b13cbf1fa868be95134dc950bb66461ce3b52
Tais993 <tijs@familiebeek.eu>
2026-06-15 13:47:51 +0200

    Added SearchSettings and PaginatedResult

--- Domain/entities/search/ImageSearchSettings.cs
+++ Domain/entities/search/ImageSearchSettings.cs
@@ -1,6 +1,6 @@
 ﻿namespace Domain.entities.search;
 
-public class ImageSearchSettings
+public class ImageSearchSettings : SearchSettings
 {
     public int? ProjectId = null;
     public string? FileNameOrNumber = null;
@@ -8,4 +8,6 @@ public class ImageSearchSettings
     public string? FileNumber = null;
     public string? FolderName = null;
     public string? FileType = null;
+
+    public bool HideRawImagesWhenJpgExists = false;
 }
