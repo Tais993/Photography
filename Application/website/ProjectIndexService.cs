353c000021651d0623a87585e2138bcd8d34f77c
Tais993 <tijs@familiebeek.eu>
2026-06-16 15:55:36 +0200

    Implemented Pagination for the Project page
    Closes #96

--- Application/website/ProjectIndexService.cs
+++ Application/website/ProjectIndexService.cs
@@ -1,4 +1,5 @@
-﻿using Application.services.interfaces;
+﻿using Application.services;
+using Application.services.interfaces;
 using Application.website.interfaces;
 using Domain.entities;
 using Domain.entities.search;
@@ -33,11 +34,22 @@ public class ProjectIndexService : IProjectIndexService
             .ThenBy(project => project.Name)
             .ToList();
 
+        PaginatedResult<Project> projectPage = PaginationService.Paginate(
+            orderedProjects,
+            new ProjectSearchSettings
+            {
+                PageNumber = request.ProjectPageNumber,
+                PageSize = request.ProjectPageSize
+            });
+
         ProjectIndexViewModel viewModel = new()
         {
-            Projects = orderedProjects,
+            ProjectPage = projectPage,
+            Projects = projectPage.Items,
             SelectedProjectId = request.SelectedProjectId,
-            ProjectCount = _projectService.GetProjectCount()
+            ProjectCount = _projectService.GetProjectCount(),
+            ProjectPageNumber = projectPage.PageNumber,
+            ProjectPageSize = projectPage.PageSize
         };
 
         if (request.SelectedProjectId is not null)
@@ -47,7 +59,7 @@ public class ProjectIndexService : IProjectIndexService
             return viewModel;
         }
 
-        viewModel.SelectedProject = orderedProjects.FirstOrDefault();
+        viewModel.SelectedProject = projectPage.Items.FirstOrDefault();
         viewModel.SelectedProjectId = viewModel.SelectedProject?.Id;
 
         return viewModel;
