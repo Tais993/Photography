e3f1733d918b1a7b5fadcb83ca3da55ac6d499eb
Tais993 <tijs@familiebeek.eu>
2026-06-16 12:34:36 +0200

    Reworked part of the selection index to use an application level service instead

---
+++ Domain/website/ImageViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.website;
+
+public class ImageViewModel
+{
+    public bool Selected { get; init; }
+    public int? ImageId { get; init; }
+    public string? FileType { get; init; }
+    public string? FileName { get; init; }
+    public string? RelationalFilePath { get; init; }
+}
---
+++ Domain/website/SelectionIndexRequestModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.website;
+
+public class SelectionIndexRequest
+{
+    public int? SelectedProjectId;
+    public int? SelectedImageId;
+    public string? Search;
+    public string? FolderName;
+    public string? FileType;
+    public int PageSize;
+    public int PageNumber;
+}
