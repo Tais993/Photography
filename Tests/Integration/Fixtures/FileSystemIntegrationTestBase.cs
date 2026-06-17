1c7781f0926b054a1a8d0c09246ed565dac5e35f
Tais993 <tijs@familiebeek.eu>
2026-06-17 14:30:24 +0200

    Added TestBases for file-system and project-file-system services/usages.
    #113

---
+++ Tests/Integration/Fixtures/FileSystemIntegrationTestBase.cs
@@ -0,0 +1,99 @@
+﻿using Application;
+
+namespace Tests.Integration.Fixtures;
+
+public abstract class FileSystemIntegrationTestBase : IntegrationTestBase
+{
+    protected string TemporaryDirectory { get; private set; } = null!;
+
+    [SetUp]
+    public void FileSystemSetUp()
+    {
+        TemporaryDirectory = Path.Combine(
+            Path.GetTempPath(),
+            "PhotographyIntegrationTests",
+            Guid.NewGuid().ToString());
+
+        Directory.CreateDirectory(TemporaryDirectory);
+    }
+
+    [TearDown]
+    public void FileSystemTearDown()
+    {
+        if (!Directory.Exists(TemporaryDirectory))
+        {
+            return;
+        }
+
+        Directory.Delete(TemporaryDirectory, true);
+    }
+
+    protected string CreateDirectory(params string[] relativePathParts)
+    {
+        string directoryPath = GetTemporaryPath(relativePathParts);
+
+        Directory.CreateDirectory(directoryPath);
+
+        return directoryPath;
+    }
+
+    protected string CreateFile(string relativeFilePath, string content = "")
+    {
+        string filePath = GetTemporaryPath(relativeFilePath);
+
+        string? directoryPath = Path.GetDirectoryName(filePath);
+
+        if (!string.IsNullOrWhiteSpace(directoryPath))
+        {
+            Directory.CreateDirectory(directoryPath);
+        }
+
+        File.WriteAllText(filePath, content);
+
+        return filePath;
+    }
+
+    protected string CreateFile(string[] relativePathParts, string content = "")
+    {
+        string filePath = GetTemporaryPath(relativePathParts);
+
+        string? directoryPath = Path.GetDirectoryName(filePath);
+
+        if (!string.IsNullOrWhiteSpace(directoryPath))
+        {
+            Directory.CreateDirectory(directoryPath);
+        }
+
+        File.WriteAllText(filePath, content);
+
+        return filePath;
+    }
+
+    protected string CreateProjectInfoFile(int projectId)
+    {
+        return CreateFile(Constants.ProjectInfoFile, projectId.ToString());
+    }
+
+    protected string GetTemporaryPath(params string[] relativePathParts)
+    {
+        return Path.Combine(
+            new[] { TemporaryDirectory }
+                .Concat(relativePathParts)
+                .ToArray());
+    }
+
+    protected bool TemporaryFileExists(params string[] relativePathParts)
+    {
+        return File.Exists(GetTemporaryPath(relativePathParts));
+    }
+
+    protected bool TemporaryDirectoryExists(params string[] relativePathParts)
+    {
+        return Directory.Exists(GetTemporaryPath(relativePathParts));
+    }
+
+    protected string ReadTemporaryFile(params string[] relativePathParts)
+    {
+        return File.ReadAllText(GetTemporaryPath(relativePathParts));
+    }
+}
