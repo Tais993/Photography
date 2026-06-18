6d6ced40fc16a8fc56fa98564ded5690d28e42ac
Tais993 <49957334+Tais993@users.noreply.github.com>
2026-06-18 09:31:49 +0200

    Revert "Added IPaginatedResult interface and PaginationService's Paginate has been simplified"
    This reverts commit d500a8e244da3d40ba481029a16faa50efcc7b04.

--- Website/Pages/Selection/Index.cshtml.cs
+++ Website/Pages/Selection/Index.cshtml.cs
@@ -36,8 +36,8 @@ public class IndexModel : PageModel
     [BindProperty(SupportsGet = true)] public string? FolderName { get; set; }
     [BindProperty(SupportsGet = true)] public string? FileType { get; set; }
 
-    [BindProperty(SupportsGet = true)] public int ImagePageSize { get; set; }
-    [BindProperty(SupportsGet = true)] public int ImagePageNumber { get; set; }
+    [BindProperty(SupportsGet = true)] public int PageSize { get; set; }
+    [BindProperty(SupportsGet = true)] public int PageNumber { get; set; }
 
     public PageResult OnGet()
     {
@@ -50,8 +50,8 @@ public class IndexModel : PageModel
             Search = Search,
             FolderName = FolderName,
             FileType = FileType,
-            PageSize = ImagePageSize,
-            PageNumber = ImagePageNumber
+            PageSize = PageSize,
+            PageNumber = PageNumber
         });
 
         ImagePage = viewModel.ImagePage;
@@ -62,8 +62,8 @@ public class IndexModel : PageModel
         SelectedProjectId = viewModel.SelectedProjectId;
         SelectedImageId = viewModel.SelectedImageId;
         ProjectImageCount = viewModel.ProjectImageCount;
-        ImagePageNumber = viewModel.PageNumber;
-        ImagePageSize = viewModel.PageSize;
+        PageNumber = viewModel.PageNumber;
+        PageSize = viewModel.PageSize;
 
         return Page();
     }
