0e87a0e2ecea843d3a31efdf402939c748b47920
Tais993 <tijs@familiebeek.eu>
2026-06-19 11:22:15 +0200

    Added ProjectFolder entities

---
+++ Domain/entities/ProjectFolder.cs
@@ -0,0 +1,21 @@
+﻿namespace Domain.entities;
+
+public class ProjectFolder
+{
+    public ProjectFolder(ProjectFolderRole role, string metadataKey, string folderName, string relativePath, string absolutePath, bool exists)
+    {
+        Role = role;
+        MetadataKey = metadataKey;
+        FolderName = folderName;
+        RelativePath = relativePath;
+        AbsolutePath = absolutePath;
+        Exists = exists;
+    }
+
+    public ProjectFolderRole Role { get; }
+    public string MetadataKey { get; }
+    public string FolderName { get; }
+    public string RelativePath { get; }
+    public string AbsolutePath { get; }
+    public bool Exists { get; }
+}
