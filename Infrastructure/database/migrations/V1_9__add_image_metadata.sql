d1d49a9a6181a7346a8be0f6edd47dbeca726fca
Tais993 <tijs@familiebeek.eu>
2026-06-24 22:40:34 +0200

    SQL migration en ImageMetadata entity

---
+++ Infrastructure/database/migrations/V1_9__add_image_metadata.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE image_metadata
+(
+    image_id       integer      NOT NULL,
+    metadata_key   varchar(255) NOT NULL,
+    metadata_value VARCHAR(255),
+
+    PRIMARY KEY (image_id, metadata_key),
+
+    FOREIGN KEY (image_id) REFERENCES image (id) ON DELETE CASCADE,
+    FOREIGN KEY (metadata_key) REFERENCES metadata (metadata_key) ON DELETE CASCADE
+);
