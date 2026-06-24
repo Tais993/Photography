68b88982fc82b8c2a5cc66504ce7f42e11273b9c
Tais993 <tijs@familiebeek.eu>
2026-06-24 12:27:43 +0200

    Added cache reset to application-reset functionality

--- Application/services/ApplicationResetService.cs
+++ Application/services/ApplicationResetService.cs
@@ -2,6 +2,7 @@
 using Application.interfaces.infrastructure.services;
 using Application.interfaces.services;
 using Application.interfaces.services.project;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using static Application.Constants;
 
@@ -11,26 +12,30 @@ public class ApplicationResetService : IApplicationResetService
 {
     private readonly IProjectService _projectService;
     private readonly IDatabaseResetService _databaseResetService;
+    private readonly IConfiguration _configuration;
     private readonly IFiles _files;
     private readonly ILogger<ApplicationResetService> _logger;
 
-    public ApplicationResetService(IProjectService projectService, IFiles files, ILogger<ApplicationResetService> logger, IDatabaseResetService databaseResetService)
+    public ApplicationResetService(IProjectService projectService, IFiles files, ILogger<ApplicationResetService> logger,
+        IDatabaseResetService databaseResetService, IConfiguration configuration)
     {
         _projectService = projectService;
         _files = files;
         _logger = logger;
         _databaseResetService = databaseResetService;
+        _configuration = configuration;
     }
 
     public void ResetApplicationData(bool dryRun = false)
     {
         _logger.LogWarning("Resetting application data");
         RemoveProjectInfoFiles(dryRun);
+        ResetCache(dryRun);
         DropDatabase(dryRun);
         _logger.LogWarning("Application data reset");
     }
 
-    public void RemoveProjectInfoFiles(bool dryRun)
+    private void RemoveProjectInfoFiles(bool dryRun)
     {
         _projectService.GetAllProjects().ForEach(project =>
         {
@@ -38,8 +43,8 @@ public class ApplicationResetService : IApplicationResetService
             _logger.LogInformation("Removing project info file for project: {}", project.Name);
 
             string projectInfoPath = _files.Combine(project.Path, ProjectInfoFile);
-            
-            
+
+
             if (!_files.Exists(projectInfoPath))
             {
                 _logger.LogWarning("Project info file does not exist: {ProjectInfoPath}", projectInfoPath);
@@ -55,10 +60,34 @@ public class ApplicationResetService : IApplicationResetService
         });
     }
 
-    public void DropDatabase(bool dryRun)
+    private void ResetCache(bool dryRun)
+    {
+        string cacheRoot = _configuration.GetValue<string>(ConfigThumbnailsCachePath) ??
+                           _files.Combine(AppContext.BaseDirectory, "thumbnail-cache");
+
+        if (!_files.Exists(cacheRoot))
+        {
+            _logger.LogWarning("Cache folder does not exist: {CacheRoot}", cacheRoot);
+        }
+        else if (!dryRun)
+        {
+            if (!dryRun)
+            {
+                _files.FolderDelete(cacheRoot, true);
+                _logger.LogInformation("Deleted cache folder: {CacheRoot}", cacheRoot);
+            }
+        }
+        else
+        {
+            _logger.LogInformation("Cache folder does exist: {CacheRoot}", cacheRoot);
+            _logger.LogInformation("Dry run, not deleting cache folder: {CacheRoot}", cacheRoot);
+        }
+    }
+
+    private void DropDatabase(bool dryRun)
     {
         if (dryRun) return;
-        
+
         _databaseResetService.DropDatabase();
     }
 }
