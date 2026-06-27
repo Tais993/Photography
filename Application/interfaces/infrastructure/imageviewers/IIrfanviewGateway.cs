ae66c8d7376c0c00f84ae8b343b980f5401738de
Tais993 <tijs@familiebeek.eu>
2026-06-27 14:07:31 +0200

    Add folder support to ImageViewerGateway's
    This tries to implement folder support, with depending on the image viewer used additionally file filtering.

--- Application/interfaces/infrastructure/imageviewers/IIrfanviewGateway.cs
+++ Application/interfaces/infrastructure/imageviewers/IIrfanviewGateway.cs
@@ -2,4 +2,10 @@
 
 public interface IIrfanviewGateway : IImageViewerGateway
 {
+    /// <summary>
+    /// This opens the whole folder; depending on the image viewer used it will only display the JPGs.
+    /// </summary>
+    /// <param name="folderPath"></param>
+    /// <param name="imageTypes"></param>
+    void OpenFolder(string folderPath, HashSet<string> imageTypes);
 }
