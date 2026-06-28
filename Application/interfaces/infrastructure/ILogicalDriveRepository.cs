738e2182896ec71dfe04668cf75ec286977e774d
Tais993 <tijs@familiebeek.eu>
2026-06-28 18:53:21 +0200

    Added LogicalDriveRepository for reading  out the existing drives and their info

---
+++ Application/interfaces/infrastructure/ILogicalDriveRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.entities;
+
+namespace Application.interfaces.infrastructure;
+
+public interface ILogicalDriveRepository
+{
+    
+    /// <summary>
+    /// Returns a list of all connected drives with their name, type and availability.
+    /// </summary>
+    /// <returns></returns>
+    public List<LogicalDrive> GetAllReady();
+}
---
+++ Domain/entities/LogicalDrive.cs
@@ -0,0 +1,20 @@
+﻿namespace Domain.entities;
+
+public class LogicalDrive
+{
+    public string Name { get; set; } = string.Empty;
+    public string VolumeLabel { get; set; } = string.Empty;
+    public string RootPath { get; set; } = string.Empty;
+    public DriveType DriveType { get; set; }
+    public bool IsReady { get; set; }
+    
+    public string ImageFolderPath { get; set; } = string.Empty;
+    
+    public long TotalSize { get; set; }
+    public long TotalFreeSpace { get; set; }
+    
+    public bool HasDcimFolder { get; set; }
+    public bool HasCameraBrand { get; set; }
+    public bool HasCameraFolder { get; set; }
+    public bool IsRecommended { get; set; }
+}
