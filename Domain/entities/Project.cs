f2f388849894501944f081921c58987f858fd7f1
Tais993 <tijs@familiebeek.eu>
2026-06-30 18:30:23 +0200

    Add storage to database and project entity
    #153

--- Domain/entities/Project.cs
+++ Domain/entities/Project.cs
@@ -11,14 +11,27 @@ public class Project
         Id = id;
     }
 
+    public Project(string name, string path, DateOnly eventDate, long? storageTotalBytes,
+        long? storageLocalBytes, DateTime? storageLastCalculated, int? parentProjectId = null,
+        int? id = null) : this(name, path, eventDate, parentProjectId: parentProjectId, id: id)
+    {
+        StorageTotalBytes = storageTotalBytes;
+        StorageLocalBytes = storageLocalBytes;
+        StorageLastCalculated = storageLastCalculated;
+    }
+
     public int? Id { get; }
     public string Name { get; set; }
     public string Path { get; }
     public DateOnly EventDate { get; }
     public int? ParentProjectId { get; }
+    public long? StorageTotalBytes { get; set; }
+    public long? StorageLocalBytes { get; set; }
+    public DateTime? StorageLastCalculated { get; set; }
 
     public override string ToString()
     {
-        return $"Id: {Id}, Name: {Name}, Path: {Path}, EventDate: {EventDate}, ParentProjectId: {ParentProjectId}";
+        return
+            $"Id: {Id}, Name: {Name}, Path: {Path}, EventDate: {EventDate}, ParentProjectId: {ParentProjectId}, StorageTotalBytes: {StorageTotalBytes}, StorageLocalBytes: {StorageLocalBytes}, StorageLastCalculated: {StorageLastCalculated}";
     }
 }
---
+++ Infrastructure/database/migrations/V1_11__add_project_storage_total.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE public.project
+ADD COLUMN storage_total_bytes BIGINT,
+ADD COLUMN storage_local_bytes BIGINT,
+ADD COLUMN storage_last_calculated TIMESTAMP;
