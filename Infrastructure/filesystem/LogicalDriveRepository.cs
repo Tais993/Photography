660572fb5153b0e61b8d213ff87278bb1f786276
Tais993 <tijs@familiebeek.eu>
2026-07-01 08:38:43 +0200

    Added filter for Google Drives
    Closes #204

--- Infrastructure/filesystem/LogicalDriveRepository.cs
+++ Infrastructure/filesystem/LogicalDriveRepository.cs
@@ -19,6 +19,7 @@ public class LogicalDriveRepository : ILogicalDriveRepository
 
         List<LogicalDrive> logicalDrives = (DriveInfo.GetDrives()
             .Where(driveInfo => driveInfo.IsReady)
+            .Where(IsNotCloudDrive)
             .Select(driveInfo => new LogicalDrive
             {
                 Name = driveInfo.Name,
@@ -32,5 +33,32 @@ public class LogicalDriveRepository : ILogicalDriveRepository
 
         _logger.LogDebug("Found {Count} logical drives", logicalDrives.Count);
         return logicalDrives;
+    }
+
+
+    private static readonly string GoogleDrive = "Google Drive";
+    private static readonly string Google = "Google";
+
+    private bool IsNotCloudDrive(DriveInfo driveInfo)
+    {
+        return !(ContainsString(driveInfo, GoogleDrive) ||
+                 ContainsString(driveInfo, Google) ||
+                 ContainsGoogleDriveFolders(driveInfo));
+    }
+
+    private static bool ContainsString(DriveInfo driveInfo, string value)
+    {
+        return driveInfo.Name.Contains(value) ||
+               driveInfo.VolumeLabel.Contains(value);
+    }
+
+    private static bool ContainsGoogleDriveFolders(DriveInfo driveInfo)
+    {
+        string root = driveInfo.RootDirectory.FullName;
+
+        return Directory.Exists(Path.Combine(root, "My Drive")) ||
+               Directory.Exists(Path.Combine(root, "Mijn Drive")) ||
+               Directory.Exists(Path.Combine(root, "Shared drives")) ||
+               Directory.Exists(Path.Combine(root, "Gedeelde drives"));
     }
 }
