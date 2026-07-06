21c45200263b5df9c60358d4850893d398b16f40
Tijs <tijs@familiebeek.eu>
2026-07-06 13:05:50 +0200

    Added IProjectUpdateService for updating ALL values of a project
    Works on #219

---
+++ Application/interfaces/services/project/IProjectUpdateService.cs
@@ -0,0 +1,10 @@
+﻿using Domain.entities;
+
+namespace Application.services.project;
+
+public interface IProjectUpdateService
+{
+    void UpdateProjectByPath(string projectPath);
+    void UpdateProject(Project? project);
+    void UpdateProject(Project? project, string? projectPath = null);
+}
